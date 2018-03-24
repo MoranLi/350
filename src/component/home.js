5c803b26c669acd02966d4b287ed2f63a9952966
yuh347 <yuh347@mail.usask.ca>
2018-03-24 14:24:00 -0600

    Home is done, store half done

--- src/component/home.js
+++ src/component/home.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import './home.css'
-import homeheader from '../images/Banners/header-banner-home.jpg';
-import homefooter from '../images/Banners/footer-banner-home.jpg';
 import Header from './partials/header.js';
 import Footer from './partials/footer.js';
+import {Carousel} from 'react-bootstrap';
 
 export default class Home extends React.Component {
     render() {
         return (
             <div id="page-wrap">
         	    <Header />
-					<div className="header-banner" style={{backgroundImage: "url(" + "https://firebasestorage.googleapis.com/v0/b/project-407f3.appspot.com/o/image%2FBanners%2Fheader-banner-home.jpg?alt=media&token=a41dd0e4-6db6-45f5-a9cf-fdf13d62105e" + ")"}}>
-              		  <div className="banner-text-heading">New fall devices have arrived.</div>
-        		      <div className="banner-text"> Teach, learn, and create with Strawberry Pi.</div>
-              		  <button type="button">Start Shopping</button>
-           		</div> 
-           		<div class="feature">
-           		  <div class="feature-text">Most Popular Products</div>
-        		  <script src="./scripts/containerBuilder.js"></script> 
-          		</div>
-        		<div class="clearboth"></div>
-        
-         		<div class="footer-banner" style={{backgroundImage: "url(" + "https://firebasestorage.googleapis.com/v0/b/project-407f3.appspot.com/o/image%2FBanners%2Ffooter-banner-home.jpg?alt=media&token=db67f19c-9b93-4f1c-892e-59a58feda0f6" + ")"}}>
-           			<div class="banner-text-heading">You could say it's our jam.</div>
-              			<div class="banner-text"> <p>Learn more about why we do what we do. </p> </div>	
-           		</div>
+                <Carousel>
+                    <Carousel.Item className='slide1'>
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item className='slide2'>
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item className='slide3'>
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
            		<Footer />
            		
            		
@@ -35,11 +39,12 @@ export default class Home extends React.Component {
                 <a href="https://plus.google.com/share?url=https://simplesharebuttons.com" target="_blank">
                 <img src="https://simplesharebuttons.com/images/somacro/google.png" alt="Google" />
                 </a>
-            
+
                 <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://simplesharebuttons.com" target="_blank">
                 <img src="https://simplesharebuttons.com/images/somacro/linkedin.png" alt="LinkedIn" />
                 </a>
         	</div>
+
         );
   }
 }
--- src/component/store.js
+++ src/component/store.js
@@ -1,29 +1,51 @@
 import React from 'react';
-
-import './store.css'
 import Header from './partials/header.js';
 import Footer from './partials/footer.js';
 
-import itemsDB from '../firebase.conf'
+import {Col, Grid, Row, Thumbnail, Button} from 'react-bootstrap';
+
+const users = [
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Tomy', age: 22, gender: 'male' },
+    { username: 'Lily', age: 19, gender: 'female' },
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Tomy', age: 22, gender: 'male' },
+    { username: 'Lily', age: 19, gender: 'female' },
+    { username: 'Lucy', age: 20, gender: 'female' }
+];
 
 
 export default class Store extends React.Component {
-
-
-
     render() {
+        const usersElements = [];
+        for (let user of users) {
+            usersElements.push(
+                <Col sm={3} md={2} xs={4}>
+                    <Thumbnail src="https://firebasestorage.googleapis.com/v0/b/project-407f3.appspot.com/o/image%2FBanners%2Ffooter-banner-about.jpg?alt=media&token=506d0c0f-a7e4-47f2-a1fb-fed7ecd854a7g" >
+                        <h4>{user.username}</h4>
+                        <p>Age: {user.age}</p>
+						<Button bsStyle="default">Add to Cart</Button>
+                    </Thumbnail>
+				</Col>
+            )
+        }
+
         return (
-	        <div id="page-wrap">
+            <div id="page-wrap">
 				<Header />
-				<div class="header-banner">
-		      		  <div class="banner-text-heading">New fall devices have arrived.</div>
-		   		</div> 
-		   		<div class="feature">
-		   		  <p class="feature-main-text">Appropriately revolutionize robust scenarios without dynamic strategic theme areas. Objectively orchestrate ethical convergence after out-of-the-box niches</p>
-				  <script src="./scripts/Strawberry_Pi.js"></script>
-		  		</div> 
-		   		<Footer />
-			</div> 
-    	);
+				<br/><br/><br/><br/>
+				<Grid>
+					<Row className="show-grid">
+						{usersElements}
+					</Row>
+				</Grid>
+
+				<Footer />
+			</div>
+        )
   }
 }
